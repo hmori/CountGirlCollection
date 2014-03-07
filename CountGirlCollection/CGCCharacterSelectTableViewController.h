814ba6b90a1fad80ea7d4256a52aeed4f3004cf4
田中 賢治 <key.m4096.kung.fu@gmail.com>
2014-03-08 07:45:01 +0900

    CharacterSelectTableView を追加

---
+++ CountGirlCollection/CGCCharacterSelectTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CGCCharacterSelectTableViewController.h
+//  CountGirlCollection
+//
+//  Created by 田中 賢治 on 2014/03/08.
+//  Copyright (c) 2014年 Hidetoshi Mori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGCCharacterSelectTableViewController : UITableViewController
+
+@end
